861261d194ec717ef085d83e7f7dccc77a369cc1
benjamin kunkel <benjamink1125@gmail.com>
2022-10-18 13:14:41 -0700

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string dogName; //Decleration syntax
+
+            dogName = "Fido";
+
+            int dogAge = 7;
+
+            char firstInitial = 'F';
+            bool isHappy = true;
+            double dogWeight = 30;
+            decimal dogHeight = 12.5m;
+
+            Console.WriteLine($"My Dog's Name Is {dogName}. He's {dogAge} years old, and is {dogWeight} pounds and he is {dogHeight} inches tall");
+        }
+
+
+    }
+
+}
